e543858cece580506102d0dc26b618a6e739eba9
waqjel <w.a110@yahoo.com>
2026-09-09 01:18:41 +0200

    Replace all files with new warehouse management layout

---
+++ src/main/java/com/EnterpriseWarehouseManagementSystem/Lab3Grupp5/Lab3Grupp5Application.java
@@ -0,0 +1,13 @@
+package com.EnterpriseWarehouseManagementSystem.Lab3Grupp5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3Grupp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3Grupp5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/EnterpriseWarehouseManagementSystem/Lab3Grupp5/Lab3Grupp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EnterpriseWarehouseManagementSystem.Lab3Grupp5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3Grupp5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
